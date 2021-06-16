dd4c46af4966fce13640a9b9884921af86398444
Zufar4ik <85727102+Zufar4ik@users.noreply.github.com>
2021-06-16 21:00:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,40 +2,69 @@ import './App.css';
 import Konva from 'konva';
 import React from 'react';
 
+class SObject extends React.Component {
+  render() {
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+              // сначала создаём контейнер
+    var stage = new Konva.Stage({
+      container: 'container',  // индификатор div контейнера
+      width: width,
+      height: height
+    });
+
+    // далее создаём слой
+    var layer = new Konva.Layer({draggable: true});
+    stage.add(layer);
+
+    // создаём фигуру
+    var Line = new Konva.Shape({
+      sceneFunc: function(context) {
+        context.beginPath();
+        context.moveTo(20, 0);
+        context.lineTo(20, 400);
+        context.quadraticCurveTo(200, 200, 200, 200);
+        context.closePath();
+
+        // special Konva.js method
+        context.fillStrokeShape(this);
+      },
+      fill: '#77DD77',
+});
+
+var Line1 = new Konva.Shape({
+  sceneFunc: function(context) {
+    context.beginPath();
+    context.moveTo(400, 0);
+    context.lineTo(400, 400);
+    context.quadraticCurveTo(200, 200, 200, 200);
+    context.closePath();
+
+    // special Konva.js method
+    context.fillStrokeShape(this);
+  },
+  fill: '#77DD77',
+});
+
+    // добавляем круг на слой
+    layer.add(Line);
+    layer.add(Line1);
+
+      return (
+        <div>
+          
+        </div>
+      )
+  }
+}
 
  class App extends React.Component {
   render() {
 
-    var width = window.innerWidth;
-      var height = window.innerHeight;
-              // сначала создаём контейнер
-          var stage = new Konva.Stage({
-            container: 'container',  // индификатор div контейнера
-            width: 500,
-            height: 500
-          });
-
-          // далее создаём слой
-          var layer = new Konva.Layer();
-
-          // создаём фигуру
-          var circle = new Konva.Circle({
-            x: stage.width() / 2,
-            y: stage.height() / 2,
-            radius: 70,
-            fill: 'red',
-            stroke: 'black',
-            strokeWidth: 4
-          });
-
-          // добавляем круг на слой
-          layer.add(circle);
-
-          // добавляем слой
-          stage.add(layer);
       return (
         <div>
-          
+          <SObject/>
         </div>
       )
   }
